c50df025be5f4e7d63ffe144577ead0794062587
kerj <kerntzj@gmail.com>
2025-07-16 17:09:16 -0700

    setup dataloader via react router, store in react-query cache and read results from cache

--- app/routes.ts
+++ app/routes.ts
@@ -1,16 +1,12 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
-    route("about", "./routes/about.tsx"),
-    route("dashboard", "./routes/dashboard.tsx", [
-        // child routes rendered in <Outlet/>
-        route("details", "./routes/details.tsx"),
-        //  Example of Dynamic Routes
-        route("settings/:id", "./routes/settings.tsx"),
-
-    ]),
-
-
-
+  index("routes/home.tsx"),
+  route("about", "./routes/about.tsx"),
+  route("dashboard", "./routes/dashboard.tsx", [
+    // child routes rendered in <Outlet/>
+    route("details", "./routes/details.tsx"),
+    //  Example of Dynamic Routes
+    route("settings/:id", "./routes/settings.tsx"),
+  ]),
 ] satisfies RouteConfig;
--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,16 +1,42 @@
+import { queryOptions, useQuery } from "@tanstack/react-query";
 import type { Route } from "./+types/settings";
+import { queryClient } from "~/queryClient";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Details" },
-    { name: "description", content: "Settings" },
-  ];
+  return [{ title: "Details" }, { name: "description", content: "Settings" }];
 }
 
-export async function loader({ params }: Route.LoaderArgs) {}
+export interface MessageResponse {
+  message: string;
+}
 
-export default function Settings({
-  params,
-}: Route.ComponentProps) {
-  return <div>Hello From settings for {params.id}</div>;
+export const getMessage = async (id: string): Promise<MessageResponse> => {
+  const res = await fetch("");
+  res.json = async () => {
+    return { message: `hello ${id}` };
+  };
+  if (!res.ok) {
+    throw new Error(`Failed to fetch message: ${res.status}`);
+  }
+  const message = await res.json();
+
+  return message;
+};
+
+const messageQueryOptions = (id: string) =>
+  queryOptions<MessageResponse>({
+    queryKey: ["message", id],
+    queryFn: () => getMessage(id),
+  });
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const url = new URL(request.url);
+  const id = url.searchParams.get("id") ?? "";
+  await queryClient.ensureQueryData(messageQueryOptions(id));
+  return null;
+}
+
+export default function Settings({ params }: Route.ComponentProps) {
+  const { data } = useQuery(messageQueryOptions(params.id));
+  return <div>{data?.message ?? "Loading..."}</div>;
 }
