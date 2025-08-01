d9415937522463e36baaa7600076a4e4a3e439a5
kerj <kerntzj@gmail.com>
2025-08-01 15:58:45 -0700

    add infinite scroll to monthly table

--- app/routes/providerData/monthlyProviderData.tsx
+++ app/routes/providerData/monthlyProviderData.tsx
@@ -1,96 +1,97 @@
-import * as React from "react";
-
-import type { Route } from "./+types/monthlyProviderData";
-import type { Data, HeadCell, Order } from "~/types";
-
-
+import { TableVirtuoso, type TableComponents } from "react-virtuoso";
+import { useState, forwardRef, Fragment, useMemo } from "react";
+import EnhancedTableHead from "~/components/table/EnhancedTableHead";
 import {
   Box,
+  Checkbox,
+  Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableRow,
-  Checkbox,
 } from "@mui/material";
-
 import OutlinedFlagIcon from "@mui/icons-material/OutlinedFlag";
 import FlagIcon from "@mui/icons-material/Flag";
-
-import EnhancedTableToolbar from "~/components/table/EnhancedTableToolbar";
-import EnhancedTableHead from "~/components/table/EnhancedTableHead";
-import { getVisibleRows } from "~/utils/table";
+import type { Data, HeadCell, Order } from "~/types";
 import DatePickerViews from "~/components/DatePickerViews";
-import { queryOptions, useQuery } from "@tanstack/react-query";
-import { queryClient } from "~/queryClient";
+import EnhancedTableToolbar from "~/components/table/EnhancedTableToolbar";
+import { useSearchParams } from "react-router";
+import { getVisibleRows } from "~/utils/table";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { getCurrentDate } from "~/utils/dates";
+import { queryClient } from "~/queryClient";
+import type { Route } from "./+types/monthlyProviderData";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Monthly Provider Data" },
-    { name: "description", content: "Monthly Provider Data" },
-  ];
-}
+interface CustomTableScroller extends React.HTMLAttributes<HTMLDivElement> {}
+// Scroller must be outside the instance that renders the rest of the table
+const Scroller = forwardRef<HTMLDivElement, CustomTableScroller>(
+  ({ style, ...props }, ref) => (
+    <TableContainer
+      component={Paper}
+      style={{ ...style }}
+      {...props}
+      ref={ref}
+    />
+  )
+);
 
 export type MonthlyProviderData = {
   id: string;
   startOfMonth: string;
   providerName: string;
-  childrenBilledOver: string;
+  childrenBilledOverCapacity: string;
   childrenPlacedOverCapacity: string;
   distanceTraveled: string;
   providersWithSameAddress: string;
   overallRiskScore: number;
 };
 
-export const getMonthlyData = async (
-  date: string
-): Promise<MonthlyProviderData[]> => {
-  const res = await fetch(`http://localhost:3000/api/v1/month/${date}`, {
-    method: "GET",
-  });
+const FETCH_ROW_COUNT = 200;
 
+export const getMonthlyData = async (
+  date: string,
+  offset: string
+): Promise<any> => {
+  console.log(`http://localhost:3000/api/v1/month/${date}?offset=${offset}`);
+  const res = await fetch(
+    `http://localhost:3000/api/v1/month/${date}?offset=${offset}`,
+    {
+      method: "GET",
+    }
+  );
   if (!res.ok) {
     throw new Error("Failed to fetch message");
   }
-
   return res.json();
 };
-
-const monthlyQueryOptions = (date: string) =>
-  queryOptions<MonthlyProviderData[]>({
-    queryKey: ["monthly", date],
-    queryFn: () => getMonthlyData(date),
-    staleTime: 1000 * 60 * 5,
-  });
-
-export async function loader({ request }: Route.LoaderArgs) {
+// Since we are populating the cache we don't need to block the UI with async/await
+export function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
   const date = url.searchParams.get("date") ?? getCurrentDate();
-  await queryClient.ensureQueryData(monthlyQueryOptions(date));
+  const offset = url.searchParams.get("offset") ?? "0";
+
+  queryClient.prefetchInfiniteQuery({
+    initialPageParam: 0,
+    queryKey: ["monthlyProviderData", date],
+    queryFn: () => getMonthlyData(date, offset),
+  });
+
   return null;
 }
 
-// Provider ID
+const riskThresholds = [
+  { max: 4, min: 3, color: "red" },
+  { max: 2, min: 2, color: "orange" },
+  { max: 1, min: 0, color: "green" },
+];
 
-// Provider Name
-
-// Overall Risk Score (sum of next four columns)
-
-// Children Billed Over Capacity (binary: yes/--)
-
-// Children Placed Over Capacity (binary: yes/--)
-
-// Distance Traveled (binary: yes/--)
-
-// Providers with Same Address (binary: yes/--)
-
-// Display the latest service month by default
-
-// If any of the risk factors (e.g. Distance Traveled) have a 1, the value should be highlighted red
-// If the Overall Risk Score is ≥3, the value should be highlighted red
-// If the Overall Risk Score is =2, the value should be highlighted orange
-// If the Overall Risk Score is 1 or 0, the value should be highlighted green
+const getColor = (value: number) => {
+  const match = riskThresholds.find(
+    (threshold) => value <= threshold.max && value >= threshold.min
+  );
+  return match ? match.color : "defaultColor";
+};
 
 const headCells: readonly HeadCell[] = [
   {
@@ -112,7 +113,7 @@ const headCells: readonly HeadCell[] = [
     label: "Overall Risk Score",
   },
   {
-    id: "childrenBilledOver",
+    id: "childrenBilledOverCapacity",
     numeric: true,
     disablePadding: false,
     label: "Children Billed Over",
@@ -137,54 +138,144 @@ const headCells: readonly HeadCell[] = [
   },
 ];
 
-const riskThresholds = [
-  { max: 4, min: 3, color: "red" },
-  { max: 2, min: 2, color: "orange" },
-  { max: 1, min: 0, color: "green" },
-];
-
-function getColor(value: number) {
-  const match = riskThresholds.find(
-    (threshold) => value <= threshold.max && value >= threshold.min
+const fixedHeaderContent = () => {
+  return (
+    <TableRow>
+      {headCells.map((column, index) => (
+        <TableCell
+          key={`${column.id}+${index}`}
+          variant="head"
+          align={column.numeric || false ? "right" : "left"}
+          style={{}}
+          sx={{ backgroundColor: "background.paper" }}
+        >
+          {column.label}
+        </TableCell>
+      ))}
+    </TableRow>
   );
-  return match ? match.color : "defaultColor";
-}
+};
+
+const renderCellContent = (
+  row: MonthlyProviderData,
+  columnId: keyof any,
+  isItemSelected: boolean,
+  labelId: string,
+  key: string
+): React.ReactNode => {
+  // console.log(columnId, row);
+
+  switch (columnId) {
+    case "id":
+      return (
+        <TableCell key={key} id={labelId} scope="row" padding="none">
+          {row.id}
+        </TableCell>
+      );
+
+    case "providerName":
+      return (
+        <TableCell key={key} align="left">
+          {row.providerName}
+        </TableCell>
+      );
+
+    case "overallRiskScore":
+      return (
+        <TableCell
+          key={key}
+          align="center"
+          sx={{
+            color: getColor(row.overallRiskScore),
+          }}
+        >
+          {row.overallRiskScore}
+        </TableCell>
+      );
+
+    case "childrenBilledOverCapacity":
+      return (
+        <TableCell key={key} align="center">
+          {row.childrenBilledOverCapacity}
+        </TableCell>
+      );
+
+    case "distanceTraveled":
+      return (
+        <TableCell key={key} align="center">
+          {row.distanceTraveled}
+        </TableCell>
+      );
+
+    case "childrenPlacedOverCapacity":
+      return (
+        <TableCell key={key} align="center">
+          {row.childrenPlacedOverCapacity}
+        </TableCell>
+      );
+
+    case "providersWithSameAddress":
+      return (
+        <TableCell key={key} align="center">
+          {row.providersWithSameAddress}
+        </TableCell>
+      );
+
+    default:
+      return null;
+  }
+};
 
 export default function MonthlyProviderData({ params }: Route.ComponentProps) {
-  const [order, setOrder] = React.useState<Order>("desc");
-  const [orderBy, setOrderBy] = React.useState<keyof Data>("overallRiskScore");
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
-  const options = React.useMemo(
-    () => monthlyQueryOptions(params.date),
-    [params.date]
-  );
+  const [selected, setSelected] = useState<readonly string[]>([]);
+  const [order, setOrder] = useState<Order>("desc");
+  const [orderBy, setOrderBy] = useState<keyof any>("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const offset = searchParams.get("offset") || "0";
 
-  const { data, isLoading, error } = useQuery(options);
+  const { data, fetchNextPage, isFetching, isLoading } = useInfiniteQuery({
+    queryKey: ["monthlyProviderData", params.date],
+    queryFn: async () => getMonthlyData(params.date, offset),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, pages) => {
+      if (!lastPage || typeof lastPage !== "object") return undefined;
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+      if (!Array.isArray(lastPage) || lastPage.length < FETCH_ROW_COUNT)
+        return undefined;
 
-  if (!data) {
-    return <div>No data available</div>;
-  }
+      return pages.length * FETCH_ROW_COUNT;
+    },
+    staleTime: 1000 * 60 * 10, // 10-Min
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  });
 
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data
-  ) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelected = data.map((n) => n.id);
-      setSelected(newSelected);
-      return;
+  const items = data?.pages.flat() || [];
+  const visibleRows = useMemo(() => {
+    if (Array.isArray(items)) {
+      return items && getVisibleRows(items, order, orderBy);
     }
-    setSelected([]);
+    return [];
+  }, [items, order, orderBy]);
+
+  const rowContent = (index: number, row: MonthlyProviderData) => {
+    const isItemSelected = selected.includes(row.id);
+    const labelId = `enhanced-table-checkbox-${index}`;
+    return (
+      <Fragment>
+        {headCells.map((column, index) => {
+          const key = `${index}-${column.id}-${row[column.id]}`;
+          return renderCellContent(
+            row,
+            column.id,
+            isItemSelected,
+            labelId,
+            key
+          );
+        })}
+      </Fragment>
+    );
   };
 
   const handleClick = (event: React.MouseEvent<unknown>, id: string) => {
@@ -206,10 +297,97 @@ export default function MonthlyProviderData({ params }: Route.ComponentProps) {
     setSelected(newSelected);
   };
 
-  const visibleRows = getVisibleRows(data, order, orderBy);
+  const VirtualTableRow = forwardRef<HTMLTableRowElement, any>(
+    ({ style, item, ...rest }, ref) => {
+      // Check boxes and such need handled by a context @_@
+      const isItemSelected = selected.includes(item.id);
+      const labelId = `enhanced-table-checkbox-${rest["data-index"]}`;
+      return (
+        <TableRow
+          hover
+          onClick={(event) => handleClick(event, item.id)}
+          role="checkbox"
+          aria-checked={isItemSelected}
+          tabIndex={-1}
+          key={item.id}
+          sx={{ cursor: "pointer", ...style }}
+          selected={isItemSelected}
+          ref={ref}
+          {...rest}
+        >
+          <td>
+            <Checkbox
+              color="primary"
+              checked={isItemSelected}
+              inputProps={{
+                id: item.id,
+                "aria-labelledby": labelId,
+              }}
+              icon={<OutlinedFlagIcon />}
+              checkedIcon={<FlagIcon />}
+            />
+          </td>
+          {rest.children}
+        </TableRow>
+      );
+    }
+  );
+
+  //   // will eventually requery
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelected = items.map((n) => n.id);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const VirtuosoTableComponents: TableComponents<any> = {
+    Scroller,
+    Table: (props) => <Table {...props} />,
+    TableHead: forwardRef<HTMLTableSectionElement>((props: any, ref) => (
+      <EnhancedTableHead
+        {...props}
+        numSelected={selected.length}
+        order={order}
+        orderBy={orderBy}
+        onSelectAllClick={handleSelectAllClick}
+        onRequestSort={handleRequestSort}
+        rowCount={items?.length || 0}
+        headCells={headCells}
+        ref={ref}
+      />
+    )),
+    TableRow: VirtualTableRow,
+    TableBody: forwardRef<HTMLTableSectionElement>((props, ref) => (
+      <TableBody {...props} ref={ref} />
+    )),
+  };
+
+  const loadMore = () => {
+    if (isFetching || isLoading) {
+      return;
+    }
+    console.log("load-more");
+    const offset = searchParams.get("offset");
+
+    const nextOffset = Number(offset) + FETCH_ROW_COUNT;
+    setSearchParams(() => ({ offset: String(nextOffset) }));
+    fetchNextPage();
+  };
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <>
       <Box sx={{ my: 4, display: "flex", alignItems: "center", gap: 2 }}>
         <DatePickerViews
           label={'"month" and "year"'}
@@ -217,75 +395,16 @@ export default function MonthlyProviderData({ params }: Route.ComponentProps) {
         />
         <EnhancedTableToolbar />
       </Box>
-      <TableContainer>
-        <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
-          <EnhancedTableHead
-            numSelected={selected.length}
-            order={order}
-            orderBy={orderBy}
-            onSelectAllClick={handleSelectAllClick}
-            onRequestSort={handleRequestSort}
-            rowCount={data.length}
-            headCells={headCells}
-          />
-          <TableBody>
-            {visibleRows.map((row, index) => {
-              const isItemSelected = selected.includes(row.id);
-              const labelId = `enhanced-table-checkbox-${index}`;
 
-              return (
-                <TableRow
-                  hover
-                  onClick={(event) => handleClick(event, row.id)}
-                  role="checkbox"
-                  aria-checked={isItemSelected}
-                  tabIndex={-1}
-                  key={row.id}
-                  selected={isItemSelected}
-                  sx={{ cursor: "pointer" }}
-                >
-                  <TableCell padding="checkbox">
-                    <Checkbox
-                      color="primary"
-                      checked={isItemSelected}
-                      inputProps={{
-                        "aria-labelledby": labelId,
-                      }}
-                      icon={<OutlinedFlagIcon />} // unchecked state
-                      checkedIcon={<FlagIcon />} // checked state
-                    />
-                  </TableCell>
-                  <TableCell
-                    // component="th"
-                    id={labelId}
-                    scope="row"
-                    padding="none"
-                  >
-                    {row.id}
-                  </TableCell>
-                  <TableCell align="left">{row.providerName}</TableCell>
-                  <TableCell
-                    align="center"
-                    sx={{
-                      color: getColor(row.overallRiskScore),
-                    }}
-                  >
-                    {row.overallRiskScore}
-                  </TableCell>
-                  <TableCell align="center">{row.childrenBilledOver}</TableCell>
-                  <TableCell align="center">
-                    {row.childrenPlacedOverCapacity}
-                  </TableCell>
-                  <TableCell align="center">{row.distanceTraveled}</TableCell>
-                  <TableCell align="center">
-                    {row.providersWithSameAddress}
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+      <TableVirtuoso
+        style={{ height: "400px" }}
+        data={visibleRows}
+        endReached={loadMore}
+        fixedHeaderContent={fixedHeaderContent}
+        increaseViewportBy={FETCH_ROW_COUNT}
+        itemContent={rowContent}
+        components={VirtuosoTableComponents}
+      />
+    </>
   );
 }
