4c16dca6c35e62c7ed5068ae3b82c05b21b0ac10
kerj <kerntzj@gmail.com>
2025-10-02 14:09:28 -0700

    update base annual table type, update YearorRange to update year param in address bar

--- app/env.ts
+++ app/env.ts
@@ -1,19 +1,21 @@
 /* eslint-disable node/no-process-env */
-import { z } from "zod/v4";
+import { z } from 'zod/v4';
 
 const envSchema = z.object({
-  ENV: z.enum(["development", "production", "test"]).default("development"),
-  VITE_API_ROOT_API_URL: z.string().nonempty()
+  ENV: z.enum(['development', 'production', 'test']).default('development'),
+  VITE_API_ROOT_API_URL: z.string().nonempty(),
+  STATE_NAME: z.string().nonempty(),
 });
 
 try {
   envSchema.parse(process.env);
-}
-catch (error) {
+} catch (error) {
   if (error instanceof z.ZodError) {
-    console.error("Missing environment variables:", error.issues.flatMap(issue => issue.path));
-  }
-  else {
+    console.error(
+      'Missing environment variables:',
+      error.issues.flatMap(issue => issue.path)
+    );
+  } else {
     console.error(error);
   }
   process.exit(1);
