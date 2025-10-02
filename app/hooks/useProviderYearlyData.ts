a0c56de0677ff605aef134708f3adb5af739dd38
kerj <kerntzj@gmail.com>
2025-10-02 14:13:11 -0700

    add new useProviderYearlyData hook, support infinite scroll, filters

---
+++ app/hooks/useProviderYearlyData.ts
@@ -0,0 +1,46 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import {
+  FETCH_ROW_COUNT,
+  getAnnualData,
+  type ProviderFilters,
+} from '~/components/services/providerDataServices';
+import type { AnnualData } from '~/types';
+
+export const useProviderYearlyData = (
+  year: string,
+  offset: string,
+  filters: Partial<ProviderFilters>,
+  initialOffset?: string | number
+) => {
+  const initOffset = Number(initialOffset) || 0;
+  const cityFilters = filters.cities ? filters.cities : [];
+  // TODO: as we add filters we should update the with them!!
+  // update the loader query key to match here
+  const queryKey = ['annualProviderData', year, filters.flagStatus, ...cityFilters];
+  return useInfiniteQuery<AnnualData[]>({
+    queryKey: queryKey,
+    queryFn: async ({ pageParam }) => {
+      // pageParam defined by getNextPageParam below, offset should only come from the dataLoader
+      const pageOffset = String(pageParam) || offset;
+      // ensure we don't send undefined as a value for filters
+      const reqFilters = {
+        ...(filters.flagStatus !== undefined ? { flagStatus: filters.flagStatus } : {}),
+        ...(filters.cities !== undefined ? { cities: filters.cities } : {}),
+      };
+
+      return getAnnualData(year, pageOffset, reqFilters);
+    },
+    initialPageParam: initOffset,
+    getNextPageParam: (lastPage, pages) => {
+      if (!lastPage || typeof lastPage !== 'object') return undefined;
+
+      if (!Array.isArray(lastPage) || lastPage.length < FETCH_ROW_COUNT) return undefined;
+
+      return pages.length * FETCH_ROW_COUNT;
+    },
+    staleTime: 1000 * 60 * 10, // 10-Min
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  });
+};
