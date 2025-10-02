e410a18708f8701b9da910a4b6a1baeed477f421
kerj <kerntzj@gmail.com>
2025-10-02 16:27:16 -0700

    lift query param provider to root to handle filters persisting between tab navigation

--- app/routes/providerData/providerData.tsx
+++ app/routes/providerData/providerData.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import type { Route } from './+types/providerData';
 
-import { Outlet, useLocation, useMatch } from 'react-router';
+import { Outlet, useLocation, useMatch, useParams } from 'react-router';
 import { Tabs, Tab, Box, useTheme } from '@mui/material';
 import { useNavigate } from 'react-router';
 
-import { QueryParamsProvider } from '~/contexts/queryParamContext';
+import { useQueryParams } from '~/contexts/queryParamContext';
 import ProviderDataCards from '~/components/providerData/providerDataCards';
+import { createQueryStringFromFilters } from '~/components/services/providerDataServices';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Provider Data' }, { name: 'description', content: 'providerData' }];
@@ -42,11 +43,43 @@ export default function ProviderData() {
   // get location and set the active tab
   const [activeTab, setActiveTab] = React.useState(getActiveTabByPath(location.pathname) || 0);
   const onMatchingRoute = useMatch(`${tabRoutes[activeTab].path}/*`);
+  let params = useParams();
+  const [queryParams, updateQuery] = useQueryParams();
+
   React.useEffect(() => {
     if (!onMatchingRoute) {
-      navigate(tabRoutes[activeTab].path, { relative: 'path' });
+      updateQuery({
+        key: 'offset',
+        value: '0',
+        type: 'SET',
+      });
+
+      const offset = queryParams?.get('offset') || '0';
+      const flagStatus = queryParams?.get('flagStatus') || undefined;
+      const cities = queryParams.getAll('cities') || undefined;
+      let searchParams = '';
+
+      const offsetMod = new URLSearchParams({ offset }).toString();
+      searchParams += `?${offsetMod}`;
+
+      const filters = {
+        flagStatus,
+        cities,
+      };
+
+      const queryString = createQueryStringFromFilters(filters);
+      if (queryString) {
+        searchParams += `&${queryString}`;
+      }
+      // checking the param and appending or removing the day depending on yearly or monthly tab
+      // allows filters to carry over between tabs
+      const param = !Object.hasOwn(params, 'year')
+        ? params?.date?.slice(0, params.date?.length - 3)
+        : params.year + '-01';
+
+      navigate(`${tabRoutes[activeTab].path}/${param}${searchParams}`);
     }
-  }, [activeTab]);
+  }, [activeTab, params]);
 
   const theme = useTheme();
   const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
@@ -75,9 +108,7 @@ export default function ProviderData() {
           />
         ))}
       </Tabs>
-      <QueryParamsProvider>
-        <Outlet />
-      </QueryParamsProvider>
+      <Outlet />
     </Box>
   );
 }
