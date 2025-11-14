ec584fbc0c6ece4122389f14405593bcecd04591
kerj <kerntzj@gmail.com>
2025-11-13 20:22:11 -0800

    Complete UI code for Historical Flags small TODO remains

--- app/routes/providerData/providerDetails.tsx
+++ app/routes/providerData/providerDetails.tsx
@@ -1,17 +1,24 @@
-import * as React from 'react';
 import type { Route } from './+types/providerDetails';
 
-import { Outlet, redirect, useLocation, useMatch, useParams } from 'react-router';
-import { Tabs, Tab, Box, useTheme, Card, Typography, Stack, Button } from '@mui/material';
-import { useNavigate } from 'react-router';
-
-import { useQueryParams } from '~/contexts/queryParamContext';
+import { redirect, useParams } from 'react-router';
+import { Box, useTheme, Card, Typography, Button, Backdrop, CircularProgress } from '@mui/material';
 import AuditLogTable from '~/components/providerData/auditTable';
-
 import AddIcon from '@mui/icons-material/Add';
 import { queryClient } from '~/queryClient';
-import { getProviderDetails } from '~/components/services/providerDataServices';
+import {
+  getProviderDetails,
+  getProviderInsights,
+  onSave,
+  type SaveInsightPayload,
+} from '~/components/services/providerDataServices';
 import { ProviderMetaDataSection } from '~/components/providerData/ProviderMetaDataSection';
+import FlagModal from '~/components/modals/FlagModal';
+import { useMemo, useState } from 'react';
+import DescriptionAlerts from '~/components/DescriptionAlerts';
+import FlagHistoryTable from '~/components/providerData/FlagHistoryTable';
+import { useProviderDetails } from '~/hooks/useProviderDetails';
+import { Edit } from '@mui/icons-material';
+import { useProviderInsights } from '~/hooks/useProviderInsights';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Provider Details' }, { name: 'description', content: 'providerDetails' }];
@@ -30,118 +37,165 @@ export async function loader({ params, request }: Route.LoaderArgs) {
     queryFn: async () => getProviderDetails(providerId),
   });
 
+  await queryClient.fetchQuery({
+    queryKey: ['providerInsights', providerId],
+    queryFn: async () => getProviderInsights(providerId),
+  });
+
   return null;
 }
 
-export default function ProviderData() {
-  const navigate = useNavigate();
-  const location = useLocation();
+export default function ProviderDetails() {
   let params = useParams();
-  const [queryParams, updateQuery] = useQueryParams();
+  const [alert, setAlert] = useState<{ success: string; message: string } | null>(null);
+  const [flagModalOpenId, setFlagModalOpenId] = useState<string | null>(null);
 
   const theme = useTheme();
+  const [isLoadingOverlayActive, setIsLoadingOverlayActive] = useState(false);
 
+  const { data: details, isLoading: isLoadingDetails } = useProviderDetails(params.providerId!);
+  const { data: insights, isLoading: isLoadingInsights } = useProviderInsights(params.providerId!);
+
+  const handleOnSave = async ({ insightData, action }: SaveInsightPayload) => {
+    setIsLoadingOverlayActive(true);
+    const res = await onSave({ insightData, action });
+    setIsLoadingOverlayActive(false);
+    if (res.ok) {
+      setAlert({
+        success: 'success',
+        message: 'Successfully updated record!',
+      });
+      // this is ran after we get a success from out local payload
+      setFlagModalOpenId(null);
+      // data has changed in the DB
+      queryClient.invalidateQueries({
+        queryKey: ['annualProviderData'],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['monthlyProviderData'],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['providerInsights'],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['provider'],
+      });
+    } else {
+      setAlert({
+        success: 'error',
+        message: 'An Error Occurred',
+      });
+    }
+  };
+  // TODO: fix this async ref should be ProviderInsight type
+  const check: any = useMemo(() => {
+    return {
+      providerName: details?.providerName || '',
+      providerLicensingId: details?.providerLicensingId || '',
+      ...insights,
+    };
+  }, [details, insights]);
+
+  console.log(check);
   return (
-    <Box display='flex' flexDirection='column' gap={3} sx={{ px: 3 }}>
-      <Box display='flex' gap={3}>
-        <ProviderMetaDataSection />
-
-        <Card
-          sx={{
-            flex: 1,
-            justifyContent: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-            p: 3,
-            gap: 2,
-          }}
-          variant='outlined'
+    <>
+      {isLoadingOverlayActive && (
+        <Backdrop
+          open={true}
+          sx={theme => ({
+            color: '#fff',
+            zIndex: 100000, // ensure it's on top
+          })}
         >
-          <Box display='flex' justifyContent='space-between' alignItems='center'>
-            <Typography variant='h5'>Flags</Typography>
-            <Button
-              onClick={() => {
-                console.log('Pressed Me!');
-              }}
-              sx={{
-                bgcolor: 'black',
-                color: 'white',
-                textTransform: 'none',
-              }}
-              startIcon={<AddIcon />}
-            >
-              Add Flag
-            </Button>
-          </Box>
+          <CircularProgress color='inherit' />
+        </Backdrop>
+      )}
+      <FlagModal
+        open={!!flagModalOpenId}
+        onClose={() => setFlagModalOpenId(null)}
+        onSave={handleOnSave}
+        providerData={check} // providerInsight data
+      />
 
-          <Stack direction='row' spacing={3}>
-            <Box>
-              <Typography variant='subtitle1' color={theme.palette.cusp_iron.contrastText}>
-                Provider Type
-              </Typography>
-              <Typography variant='body1'>Home Health Agency</Typography>
+      <DescriptionAlerts
+        severity={alert?.success}
+        message={alert?.message}
+        open={alert !== null}
+        handleClose={() => setAlert(null)}
+      />
+      <Box display='flex' flexDirection='column' gap={3} sx={{ px: 3 }}>
+        <Box display='flex' gap={3}>
+          <ProviderMetaDataSection data={details!} isLoading={isLoadingDetails} />
+          <Card
+            sx={{
+              flex: 1,
+              justifyContent: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              p: 3,
+              gap: 2,
+            }}
+            variant='outlined'
+          >
+            <Box display='flex' justifyContent='space-between' alignItems='center'>
+              <Typography variant='h5'>Flags</Typography>
+              <Button
+                onClick={() => {
+                  setFlagModalOpenId('123');
+                }}
+                sx={{
+                  bgcolor: 'black',
+                  color: 'white',
+                  textTransform: 'none',
+                }}
+                startIcon={insights?.is_flagged ? <Edit /> : <AddIcon />}
+              >
+                {insights?.is_flagged ? 'Update Flag' : 'Add Flag'}
+              </Button>
             </Box>
-            <Box>
-              <Typography variant='subtitle1' color={theme.palette.cusp_iron.contrastText}>
-                Phone
-              </Typography>
-              <Typography variant='body1'>(555) 123-4567</Typography>
-            </Box>
-            <Box>
-              <Typography variant='subtitle1' color={theme.palette.cusp_iron.contrastText}>
-                Last Audited
-              </Typography>
-              <Typography variant='body1'>2023-10-15</Typography>
-            </Box>
-          </Stack>
+            <FlagHistoryTable history={insights?.history || []} isLoading={isLoadingInsights} />
+          </Card>
+        </Box>
 
-          <Box>
-            <Typography variant='subtitle1' color={theme.palette.cusp_iron.contrastText}>
-              Address
-            </Typography>
-            <Typography variant='body1'>123 Medical Parkway, Springfield, IL 62704</Typography>
-          </Box>
-        </Card>
+        <Box>
+          <Card
+            sx={{
+              flex: 1,
+              justifyContent: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              p: 3,
+              gap: 2,
+            }}
+            variant='outlined'
+          >
+            <Box display='flex' justifyContent='space-between' alignItems='center'>
+              <Box>
+                <Typography variant='h5'>Audit History</Typography>
+                <Typography variant='body1' color={theme.palette.cusp_iron.contrastText}>
+                  {' '}
+                  Record of previous audits and findings
+                </Typography>
+              </Box>
+              <Button
+                onClick={() => {
+                  console.log('Pressed Me!');
+                }}
+                sx={{
+                  bgcolor: 'black',
+                  color: 'white',
+                  textTransform: 'none',
+                }}
+                startIcon={<AddIcon />}
+              >
+                Add Audit Log
+              </Button>
+            </Box>
+
+            <AuditLogTable />
+          </Card>
+        </Box>
       </Box>
-
-      <Box>
-        <Card
-          sx={{
-            flex: 1,
-            justifyContent: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-            p: 3,
-            gap: 2,
-          }}
-          variant='outlined'
-        >
-          <Box display='flex' justifyContent='space-between' alignItems='center'>
-            <Box>
-              <Typography variant='h5'>Audit History</Typography>
-              <Typography variant='body1' color={theme.palette.cusp_iron.contrastText}>
-                {' '}
-                Record of previous audits and findings
-              </Typography>
-            </Box>
-            <Button
-              onClick={() => {
-                console.log('Pressed Me!');
-              }}
-              sx={{
-                bgcolor: 'black',
-                color: 'white',
-                textTransform: 'none',
-              }}
-              startIcon={<AddIcon />}
-            >
-              Add Audit Log
-            </Button>
-          </Box>
-
-          <AuditLogTable />
-        </Card>
-      </Box>
-    </Box>
+    </>
   );
 }
