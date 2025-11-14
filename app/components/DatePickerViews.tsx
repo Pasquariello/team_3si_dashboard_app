fc57e6e994f56bc40d46b80018c8058acf3aaf31
kerj <kerntzj@gmail.com>
2025-11-13 20:14:50 -0800

    rework flag modal to work in provider details page, make DatePickerViews usable there too

--- app/components/DatePickerViews.tsx
+++ app/components/DatePickerViews.tsx
@@ -1,73 +1,30 @@
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { useParams, useNavigate } from 'react-router';
-import { useState, useEffect } from 'react';
-import { parseISO, isValid } from 'date-fns';
-import { createQueryStringFromFilters } from './services/providerDataServices';
-import { useQueryParams } from '~/contexts/queryParamContext';
+import { useState } from 'react';
+import { parseISO } from 'date-fns';
 
 interface DatePickerViewsProps {
+  initialDate: string;
   label: string;
   views: Array<'year' | 'month'>;
+  handler?: (newDate: Date) => void;
 }
-
-export default function DatePickerViews({ label, views }: DatePickerViewsProps) {
-  const { date } = useParams();
-  const navigate = useNavigate();
-
-  const [value, setValue] = useState<Date | null>(null);
-  const [queryParams, updateQuery] = useQueryParams();
-
-  // Load initial date from path param
-  useEffect(() => {
-    if (date) {
-      const parsed = parseISO(date);
-      if (isValid(parsed)) {
-        setValue(parsed);
-      }
-    }
-  }, [date]);
-
+export default function DatePickerViews({
+  initialDate,
+  label,
+  views,
+  handler,
+}: DatePickerViewsProps) {
+  const [value, setValue] = useState<Date | null>(parseISO(initialDate));
   const handleChange = (newDate: Date | null) => {
     // clean/verify date before setting
-    setValue(newDate);
-    if (newDate) {
-      const year = newDate.getFullYear();
-      const month = String(newDate.getMonth() + 1).padStart(2, '0');
-
-      const pathname = location.pathname;
-      const updatedPath = pathname
-        .split('/')
-        .map(segment => (segment === date ? `${year}-${month}` : segment))
-        .join('/');
-
-      updateQuery({
-        key: 'offset',
-        value: '0',
-        type: 'SET',
-      });
-
-      const offset = queryParams?.get('offset') || '0';
-      const flagStatus = queryParams?.get('flagStatus') || undefined;
-      const cities = queryParams.getAll('cities') || undefined;
-      let searchParams = '';
-
-      const offsetMod = new URLSearchParams({ offset }).toString();
-      searchParams += `?${offsetMod}`;
-
-      const filters = {
-        flagStatus,
-        cities,
-      };
-
-      const queryString = createQueryStringFromFilters(filters);
-      if (queryString) {
-        searchParams += `&${queryString}`;
+    setValue(() => {
+      if (newDate && handler) {
+        handler(newDate);
       }
-
-      navigate(`${updatedPath}${searchParams}`);
-    }
+      return newDate;
+    });
   };
 
   return (
